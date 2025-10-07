bc43ad278251fc80ab627172741586fe8ad0dba3
Pablo Chantada <pablo.chantada@udc.es>
2025-10-07 17:24:26 +0200

    fix: clipboard based on OS, uses wayland clipboard for linux

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,16 +1,23 @@
 -- Clipboard
 vim.opt.clipboard = "unnamedplus"
 
-vim.g.clipboard = {
-  name = 'wl-clipboard',
-  copy = {
-    ['+'] = 'wl-copy',
-    ['*'] = 'wl-copy',
-  },
-  paste = {
-    ['+'] = 'wl-paste',
-    ['*'] = 'wl-paste',
-  },
-  cache_enabled = 0,
-}
+-- O.S
+local uname = vim.loop.os_uname().sysname
 
+if uname == "Linux" then
+    vim.g.clipboard = {
+      name = 'wl-clipboard',
+      copy = {
+        ['+'] = 'wl-copy',
+        ['*'] = 'wl-copy',
+      },
+      paste = {
+        ['+'] = 'wl-paste',
+        ['*'] = 'wl-paste',
+      },
+      cache_enabled = 0,
+    }
+else
+    -- For macOS and Windows use default clipboard
+    vim.g.clipboard = nil
+end
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,6 @@
 -- Transparent
--- vim.api.nvim_set_hl(0, "Normal", { bg = "none" } )
--- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" } )
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" } )
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" } )
 
 
 -- Kanagawa theme
