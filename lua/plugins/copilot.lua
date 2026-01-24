c3b90898bb0f23edb4230ee039e5034364578af6
Pablo Chantada Saborido <pablo.chantada@udc.es>
2026-01-24 20:16:04 +0100

    feat: update copilot shortcuts

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,23 +1,20 @@
 -- Copilot
 return {
-    {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  config = function()
-    require("copilot").setup({
-      suggestion = { enabled = false },
-      panel = { enabled = false },
-    })
-  end,
-    },
-
-    -- Copilot CMP
-    {
-      "zbirenbaum/copilot-cmp",
-      after = { "copilot.lua" },
-      config = function ()
-        require("copilot_cmp").setup()
-      end
-    }
+  "CopilotC-Nvim/CopilotChat.nvim",
+  branch = "canary",  -- For latest features
+  dependencies = {
+    { "zbirenbaum/copilot.lua" },  -- Base Copilot (or github/copilot.vim)
+    { "nvim-lua/plenary.nvim" },
+    { "nvim-telescope/telescope.nvim" },  -- Optional for actions
+  },
+  opts = {
+    window = { layout = "float" },  -- Or "vsplit"
+    auto_follow_cursor = false,
+  },
+  keys = {
+    { "<leader>cc", ":CopilotChatToggle<CR>", desc = "Toggle Copilot Chat" },
+    { "<leader>cce", ":CopilotChatExplain<CR>", desc = "Explain code" },
+    { "<leader>ccf", ":CopilotChatFix<CR>", desc = "Fix code" },
+  },
 }
+
